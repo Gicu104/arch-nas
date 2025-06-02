2606efbf0e2502abeed4972c88d1635e619fc879
Gicu104 <122215709+Gicu104@users.noreply.github.com>
2025-06-02 15:10:19 +0200

    Update check-bandwidth.sh
    changed interface

--- scripts/monitoring/check-bandwidth.sh
+++ scripts/monitoring/check-bandwidth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Check if daily RX+TX > threshold (e.g., 3 GB)
 THRESHOLD_MB=3000
-INTERFACE="eth0"  # or check `vnstat --iflist`
+INTERFACE="enp1s0"  # or check `vnstat --iflist`
 
 TOTAL_MB=$(vnstat --oneline -i $INTERFACE | awk -F\; '{print $(NF-3) + $(NF-1)}')
 
