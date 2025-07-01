b848895ae603ea6462fd929b02d1a470d7e7ccc7
Gicu104 <122215709+Gicu104@users.noreply.github.com>
2025-07-01 16:30:48 +0200

    Update check-backup.sh
    Modified Path to look at one specifica backup,
    IT doesnt cover all backups

--- scripts/monitoring/check-backup.sh
+++ scripts/monitoring/check-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SNAPSHOT_DIR="/mnt/data/backup/snapshots/$(date +%F)"
+SNAPSHOT_DIR="/mnt/data/backup/cloudshare/$(date +%F)"
 
 if [ ! -d "$SNAPSHOT_DIR" ]; then
   echo "[ALERT] Backup missing for $(date +%F) at $(date)" >> /var/log/backup-alert.log
