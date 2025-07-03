2053e71b5b0502f10f21f798c0d177030c60cef5
Gicu104 <122215709+Gicu104@users.noreply.github.com>
2025-07-03 12:26:27 +0200

    Update check-syncthing.sh

--- scripts/monitoring/check-syncthing.sh
+++ scripts/monitoring/check-syncthing.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if ! systemctl is-active --quiet syncthing@gicu; then
-    echo "Syncthing is down at $(date)" >> /var/log/syncthing-alert.log
+    echo "Syncthing is down at $(date)" >> /var/log/alerts/syncthing-alert.log
 fi
