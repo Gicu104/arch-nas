c85545ee37c52cd64c27975ae94a9326f54c6627
Gicu104 <122215709+Gicu104@users.noreply.github.com>
2025-07-03 12:26:05 +0200

    Update check-bandwidth.sh

--- scripts/monitoring/check-bandwidth.sh
+++ scripts/monitoring/check-bandwidth.sh
@@ -6,5 +6,5 @@ INTERFACE="enp1s0"  # or check `vnstat --iflist`
 TOTAL_MB=$(vnstat --oneline -i $INTERFACE | awk -F\; '{print $(NF-3) + $(NF-1)}')
 
 if (( $(echo "$TOTAL_MB > $THRESHOLD_MB" | bc -l) )); then
-  echo "[ALERT] Bandwidth usage high: ${TOTAL_MB}MB used on $(date)" >> /var/log/bandwidth-alert.log
+  echo "[ALERT] Bandwidth usage high: ${TOTAL_MB}MB used on $(date)" >> /var/log/alerts/bandwidth-alert.log
 fi
