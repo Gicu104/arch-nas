5d0869833ad9e7ea41291798dd732d849187a9d3
Gicu104 <122215709+Gicu104@users.noreply.github.com>
2025-07-08 11:27:25 +0200

    Update check-backup.sh

--- scripts/monitoring/check-backup.sh
+++ scripts/monitoring/check-backup.sh
@@ -7,7 +7,7 @@ TODAY=$(date +%F)
 MOD_DATE=$(stat "$BACKUP_FILE" | grep '^Modify:' | cut -d' ' -f2)
 
 if [ "$MOD_DATE" = "$TODAY" ]; then
-    echo "Backup file was modified today ($TODAY)."
+    echo "Backup file was complete today ($TODAY)."
 else
     echo "[ALERT] Backup script did not complete for $TODAY at $(date)" >> /var/log/alerts/backup-alert.log
     exit 1
