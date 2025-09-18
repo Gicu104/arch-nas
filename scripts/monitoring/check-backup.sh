3c5cda1d29b36c31f5b55fcb893c29ff312e646b
Gicu104 <122215709+Gicu104@users.noreply.github.com>
2025-09-18 10:50:58 +0200

    Update check-backup.sh
    Reverted check for folder not mark file

--- scripts/monitoring/check-backup.sh
+++ scripts/monitoring/check-backup.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
+SNAPSHOT_DIR="/mnt/data/backup/cloudshare/$(date +%F)*"
 
-BACKUP_FILE="/mnt/data/backup/last-successfull-backup"
-TODAY=$(date +%F)
-
-# Extract the modification date of the backup file (YYYY-MM-DD)
-MOD_DATE=$(stat "$BACKUP_FILE" | grep '^Modify:' | cut -d' ' -f2)
-
-if [ "$MOD_DATE" = "$TODAY" ]; then
-    echo "Backup file was complete today ($TODAY)."
-else
-    echo "[ALERT] Backup script did not complete for $TODAY at $(date)" >> /var/log/alerts/backup-alert.log
-    exit 1
+if [ ! -d "$SNAPSHOT_DIR" ]; then
+  echo "[ALERT] Backup missing for $(date +%F) at $(date)" >> /var/log/alerts/backup-alert.log
 fi
